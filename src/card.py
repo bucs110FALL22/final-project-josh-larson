7d62f489c548aec99915196f8d30d9b8be54a909
Joshua Janason <112440235+JoshuaJanason@users.noreply.github.com>
2022-11-12 03:45:01 +0000

    Milestone 3

--- src/card.py
+++ src/card.py
@@ -10,8 +10,8 @@ class Card(pygame.sprite.Sprite):
     self.rect = self.image.get_rect()
 
     def move_up(self):
-      self.rect.y -= self.move
       self.rect.y += self.move
+      self.rect.y -= self.move
       self.rect.x += self.speed
       self.rect.x -= self.speed
   
